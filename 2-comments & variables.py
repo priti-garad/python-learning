0d1c5e3b8a1e393765c6e78955f299f12f20ca33
priti-garad <garadpriti4@gmail.com>
2026-04-19 21:40:05 +0530

    Added comments and variables file

---
+++ 2-comments & variables.py
@@ -0,0 +1,10 @@
+#This is my second program
+#learning variables
+name ="priti"
+age =21
+salary =80000
+height=5.7
+print("my name is",name)
+print("age=",age)
+print("salary=",salary)
+print("height",height)
