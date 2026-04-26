65def80f21a1ec40c7e5803a5eae2c78eb029baf
priti-garad <garadpriti4@gmail.com>
2026-04-26 19:42:00 +0530

    Added Python operators file

---
+++ 4-operators.py
@@ -0,0 +1,36 @@
+# 1.Arithmetic operator
+num1=40
+num2=8
+print(num1-num2)
+print(num1*num2)
+
+# 2.Assignment operators
+value=50
+print(value)
+value-=10
+print(value)
+
+# 3.Comparison operators
+p1=12
+p2=18
+print(p1<p2)
+
+p3=30
+p4=25
+print(p3<p4)
+
+# 4.Logical operators
+x1=5
+x2=15
+x3=25
+x4=35
+print(x1<x2 and x3<x4)
+print("OR Result :",x1>x2 and x3>x4)
+
+# 5.Identity operators
+m1=100
+m2=100
+print(m1 is m2)
+
+# 6.Membership operator
+print("check 'e' in 'mango' :",'e' in 'mango')
